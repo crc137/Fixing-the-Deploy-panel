ddbdea6dda3faf37a9058554b4e44c2db2504228
Alberto Clemente <165967398+crc137@users.noreply.github.com>
2025-06-26 04:10:34 +0300

    Create install.user.js

---
+++ install.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Fixing Dokploy Panel
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Fixing the Dokploy admin panel
+// @author       Coonlink Dev
+// @homepageURL  https://dev.coonlink.com/fixing-dokploy-panel
+// @updateURL    https://coonlink.com/fixing-dokploy-panel.user.js
+// @downloadURL  https://coonlink.com/fixing-dokploy-panel.user.js
+// @supportURL   https://dev.coonlink.com/
+// @match        https://admin.coonlink.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=coonlink.com
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
