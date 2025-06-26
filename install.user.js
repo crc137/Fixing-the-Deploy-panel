580f1ba2ed2d24c9ddfc028b9e95c2ceba3a4d40
Alberto Clemente <165967398+crc137@users.noreply.github.com>
2025-06-26 05:02:14 +0300

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -1,14 +1,56 @@
 // ==UserScript==
 // @name         Fixing Dokploy Panel
+// @name:de      Fixieren des Dokploy-Panels
+// @name:es      Arreglar panel de Dokploy
+// @name:zh      修复 Dokploy 面板
+// @name:zh-TW   修復 Dokploy 面板
+// @name:pt      Corrigir painel do Dokploy
+// @name:ru      Исправление панели Dokploy
+// @name:ja      Dokploy パネルの修正
+// @name:ko      Dokploy 패널 수정
+// @name:fr      Correction du panneau Dokploy
+// @name:tr      Dokploy Panelini Düzelt
+// @name:it      Correzione del pannello Dokploy
+// @name:pl      Naprawa panelu Dokploy
+// @name:uk      Виправлення панелі Dokploy
+// @name:fa      اصلاح پنل Dokploy
+// @name:nl      Dokploy-paneel repareren
+// @name:id      Memperbaiki panel Dokploy
+// @name:kk      Dokploy панелін түзету
+// @name:no      Fikse Dokploy-panelet
+// @name:az      Dokploy panelinin düzəldilməsi
+// @name:ml      Dokploy പാനൽ പരിഹരിക്കൽ
+
+// @description  Fixing the Dokploy admin panel
+// @description:de  Fehlerbehebung im Dokploy-Admin-Panel
+// @description:es  Arreglando el panel de administración de Dokploy
+// @description:zh  修复 Dokploy 管理面板
+// @description:zh-TW  修復 Dokploy 管理面板
+// @description:pt  Corrigindo o painel de administração do Dokploy
+// @description:ru  Исправление панели администратора Dokploy
+// @description:ja  Dokploy 管理パネルの修正
+// @description:ko  Dokploy 관리자 패널 수정
+// @description:fr  Correction du panneau d'administration Dokploy
+// @description:tr  Dokploy yönetici panelini düzeltme
+// @description:it  Correzione del pannello di amministrazione di Dokploy
+// @description:pl  Naprawa panelu administracyjnego Dokploy
+// @description:uk  Виправлення панелі адміністратора Dokploy
+// @description:fa  اصلاح پنل مدیریت Dokploy
+// @description:nl  Het beheerpaneel van Dokploy repareren
+// @description:id  Memperbaiki panel admin Dokploy
+// @description:kk  Dokploy әкімшілік панелін түзету
+// @description:no  Fikser Dokploy administrasjonspanel
+// @description:az  Dokploy idarəetmə panelinin düzəldilməsi
+// @description:ml  Dokploy അഡ്മിന് പാനൽ പരിഹരിക്കുന്നു
+
 // @namespace    http://tampermonkey.net/
 // @version      1.0.0
-// @description  Fixing the Dokploy admin panel
 // @author       Coonlink Dev
 // @homepageURL  https://github.com/crc137/Fixing-the-Deploy-panel
 // @updateURL    https://coonlink.com/fixing-dokploy-panel.user.js
 // @downloadURL  https://coonlink.com/fixing-dokploy-panel.user.js
 // @supportURL   https://github.com/crc137/Fixing-the-Deploy-panel/issues
-// @match        {url Dokploy admin panel}
+// @match        https://admin.coonlink.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=coonlink.com
 // @grant        none
 // @run-at       document-end
