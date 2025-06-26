0e950d818a940c31f194121b2451e52088b0d99e
Alberto Clemente <165967398+crc137@users.noreply.github.com>
2025-06-26 04:29:54 +0300

    Update install.user.js

--- install.user.js
+++ install.user.js
@@ -4,7 +4,7 @@
 // @version      1.0.0
 // @description  Fixing the Dokploy admin panel
 // @author       Coonlink Dev
-// @homepageURL  https://dev.coonlink.com/fixing-dokploy-panel
+// @homepageURL  https://github.com/crc137/Fixing-the-Deploy-panel
 // @updateURL    https://coonlink.com/fixing-dokploy-panel.user.js
 // @downloadURL  https://coonlink.com/fixing-dokploy-panel.user.js
 // @supportURL   https://github.com/crc137/Fixing-the-Deploy-panel/issues
